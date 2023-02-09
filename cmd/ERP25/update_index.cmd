e4f1318a50d251bafcc2b5a6cd507da9e3f2c2fa
sv.karushev <sv.karushev@tensor.ru>
2023-02-09 13:34:29 +0500

    no message

--- cmd/ERP25/update_index.cmd
+++
@@ -1,8 +0,0 @@
-SET PRODUCT=saby_ce_kedo
-SET SUBSYSTEM=ERP25
-SET DESCENT=2005006
-SET PLATFORM=80314
-
-cd ../..
-v8unpack.exe -I src\%SUBSYSTEM%PROF --index cmd\%SUBSYSTEM%\index.json  --core base --descent %DESCENT%
-pause
